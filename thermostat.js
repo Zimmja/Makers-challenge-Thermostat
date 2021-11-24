ebbbd5810631263a4d559961f360b18f235c9d41
Matt Zimmer <zimmermja@gmail.com>
2021-11-24 14:57:44 +0000

    added getTemperature, up, down and setPowerSavingMode functions to Thermostat class; all tests passing

--- thermostat.js
+++ thermostat.js
@@ -1,10 +1,33 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this.powerSaver = true
   }
 
   getTemperature = () => this.temperature;
-  
+
+  up = () => {
+    if (this.temperature < this.maxtemp()) {
+      this.temperature += 1;
+    };
+  };
+
+  down = () => {
+    if (this.temperature > 10) {
+      this.temperature -= 1;
+    };
+  };
+
+  maxtemp = () => {
+    if (this.powerSaver == true) {
+      return 25;
+    } else {
+      return 32;
+    };
+  };
+
+  setPowerSavingMode = (powerBool) => this.powerSaver = powerBool;
+
 };
 
 module.exports = Thermostat;
--- thermostat.test.js
+++ thermostat.test.js
@@ -2,7 +2,52 @@ const Thermostat = require("./thermostat");
 
 describe(".getTemperature", () => {
   it('returns the current temperature', () => {
-    let testStat = new Thermostat
+    let testStat = new Thermostat;
     expect(testStat.getTemperature()).toBe(20);
+  });
+});
+
+describe(".up", () => {
+  it('increases the temperature', () => {
+    let testStat = new Thermostat;
+    for (let i = 1; i < 3; i++ ) {
+      testStat.up()
+      expect(testStat.getTemperature()).toBe(20 + i);
+    };
+  });
+
+  it('cannot increase the temperature over the power saving maximum of 25', () => {
+    let testStat = new Thermostat;
+    testStat.setPowerSavingMode(true)
+    for (let i = 1; i < 50; i++ ) {
+      testStat.up()
+    };
+    expect(testStat.getTemperature()).toBe(25);
+  });
+
+  it('cannot increase the temperature over the non-power saving maximum of 32', () => {
+    let testStat = new Thermostat;
+    testStat.setPowerSavingMode(false)
+    for (let i = 1; i < 50; i++ ) {
+      testStat.up()
+    };
+    expect(testStat.getTemperature()).toBe(32);
+  });
+});
+
+describe(".down", () => {
+  it('decreases the temperature', () => {
+    let testStat = new Thermostat;
+    expect(testStat.getTemperature()).toBe(20);
+    testStat.down();
+    expect(testStat.getTemperature()).toBe(19);
+  });
+
+  it('cannot decrease the temperature below the minimum of 10', () => {
+    let testStat = new Thermostat;
+    for (let i = 1; i < 50; i++ ) {
+      testStat.down()
+    };
+    expect(testStat.getTemperature()).toBe(10);
   });
 });
